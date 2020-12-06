49b72f9ec102abaa24da12adf28179668318af49
Nico Rithner <nicorithner@gmail.com>
2020-12-06 14:44:21 -0700

    Start JS fundamentals Part 2

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+'use strict'; // points us to errors
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if(passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+// Strict mode reseved words:
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;
