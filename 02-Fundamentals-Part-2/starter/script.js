193acdee90a016f3b0b3b192dff658a55a293fa7
Nico Rithner <nicorithner@gmail.com>
2020-12-06 15:11:39 -0700

    Lesson 3. Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,12 +1,29 @@
 'use strict'; // points us to errors
-
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
 if(passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log('I can drive');
 
-// Strict mode reseved words:
-// const interface = 'Audio';
-// const private = 534;
-// const if = 23;
+Strict mode reseved words:
+const interface = 'Audio';
+const private = 534;
+const if = 23;
+*/
+function logger() {
+  console.log('My name is Jonas'); // funtion body
+}
+// calling, running or invoking the function.
+logger(); 
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} organges`
+  return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
