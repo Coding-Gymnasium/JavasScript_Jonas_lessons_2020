18cd8a512e31754eb42799a037bb919404fc061e
Nico Rithner <nicorithner@gmail.com>
2020-12-06 15:45:17 -0700

    Lesson 5. Arrow Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -26,22 +26,37 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
-*/
 
 // Function Declaration
+
+const age1 = calcAge1(2005);
 
 function calcAge1(birthYear) {
   return new Date().getFullYear() - birthYear;
 }
-
-const age1 = calcAge1(2005);
 console.log(age1);
 
 // Function expression
-
 const calcAge2 = function (birthYear) {
   return new Date().getFullYear() - birthYear;
 }
 
 const age2 = calcAge2(2005);
 console.log(age1, age2);
+*/
+
+// Arrow Function. 
+let year = new Date().getFullYear();
+
+const calcAge3 = birthYear => year - birthYear;
+console.log(calcAge3(2000));
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = year - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} will be 65 in ${retirement} years`
+}
+
+console.log(yearsUntilRetirement(1972, 'Nico')); // returns: Nico will be 65 in 17 years
+console.log(yearsUntilRetirement(2004, 'Noelle')); // returns: Noelle will be 65 in 49 years
