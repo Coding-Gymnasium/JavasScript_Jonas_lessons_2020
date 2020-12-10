bfc0eb1b3df6f7dcdcb2f4ee80d8fe628cc4ee48
Nico Rithner <nicorithner@gmail.com>
2020-12-10 14:31:37 -0700

    Complete Challenge 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -183,7 +183,7 @@ const years1 = [1990, 1967, 2002, 2010, 2018];
 for (let i = 0; i < years1.length; i++) {
   console.log(calcAge(years1[i]));
 }
-*/
+
 // Add elements
 const friends = ['Michael', 'Steven', 'Peter'];
 const newlength = friends.push('Jay');
@@ -217,3 +217,54 @@ if (friends.includes(friend)) {
 } else {
   console.log('No such a friend');
 }
+*/
+
+/*
+Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: 
+Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
+
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data below
+3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip 
+
+Test data: 125, 555 and 44
+
+Hint: 
+Remember that an array needs a value in each position, and that value can actually be the returned value of a function! 
+So you can just call a function as array values (so don't store the tip values in separate variables first, but right in the new array) 😉
+GOOD LUCK 😀
+*/
+
+// function calcTip(billValue) {
+//   if(billValue >= 50 && billValue <= 300) {
+//     return billValue * .15
+//   } else {
+//     return billValue * .20
+//   }
+// }
+
+// Using Terniary
+// function calcTip(billValue) {
+//   return billValue >= 50 && billValue <= 300 ? billValue * .15 : billValue * .20;
+// }
+
+// Using Arrow Function 
+const calcTip = billValue => billValue >= 50 && billValue <= 300 ? billValue * .15 : billValue * .20;
+console.log(calcTip(50));
+
+const bills = new Array(125, 555, 44);
+const tips = []
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+}
+console.log(tips);
+
+const total = [];
+for (let i = 0; i < bills.length; i++) {
+  total.push(bills[i] + tips[i]);
+}
+console.log(total);
