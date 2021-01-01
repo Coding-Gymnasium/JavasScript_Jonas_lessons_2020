70417262cd3c5f6f9fcfc43a3f82dac579a4ddbf
Nico Rithner <nicorithner@gmail.com>
2020-12-31 23:43:37 -0700

    Complete lesson 95. Hoisting and TDZ in Practice

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -38,3 +38,47 @@ const firstName = 'Jonas';
 calcAge(1991);
 // console.log(age);
 // printAge();
+*/
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDec(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow); // undefined
+// console.log(addArrow(2, 3));
+
+function addDec(a, b) {
+  return a + b;
+} // works because it's a function declaration
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+// Example
+
+if (!numProducts) deleteShoppingCart(); // executes because !numProducts is 'undefined' at this stage
+
+var numProducts = 10; // avoid using 'var' and declare variables on top
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x); // true
+console.log(y === window.y); // false
+console.log(z === window.z); // false
