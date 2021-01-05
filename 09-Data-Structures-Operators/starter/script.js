5e45ad688b37e9d6ab04db8360614ffba554a8ad
Nico Rithner <nicorithner@gmail.com>
2021-01-04 20:41:37 -0700

    Complete lesson 107. Short Circuiting (&& and ||)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -47,6 +47,36 @@ const restaurant = {
   },
 };
 
+console.log('---- OR ----');
+// Use ANY data type, return ANY data type, short-circuiting (short circuit evaluation)
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null); // returns 'Hello' because is the first one to be truth so it short-circuits the evaluation
+
+// restaurant.numGuests = 0; // a falsy value
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+console.log('---- AND ----');
+
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'Jonas'); // returns 'Hello' because is the first one to be truth so it short-circuits the evaluation
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'spinach');
+/*
 //// Rest pattern and parameters
 //// spread operator unpacks elements from an array, while Rest packs elements into an array
 
