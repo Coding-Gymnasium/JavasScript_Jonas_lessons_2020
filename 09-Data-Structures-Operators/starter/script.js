e86ff0818ab806fd444b60de7fccf45651eda0e7
Nico Rithner <nicorithner@gmail.com>
2021-01-15 14:51:10 -0700

    Complete lesson 117. Maps iterators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -564,7 +564,43 @@ console.log(new Set(staff).size); // 3
 console.log(new Set('Nicolas').size); // 7
 */
 
-// Maps
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎉'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+console.log([question.entries()]);
+console.log([...question.keys()]); //  ["question", 1, 2, 3, "correct", true, false]
+console.log([...question.values()]); // ["What is the best programming language in the world?", "C", "Java", "JavaScript", 3, "Correct 🎉", "Try again!"]
+
+/*
+///////////////////////////
+// Maps fundamentals
 
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
@@ -601,3 +637,4 @@ rest.set(document.querySelector('h1'), 'Heading');
 
 console.log(rest);
 console.log(rest.size);
+*/
