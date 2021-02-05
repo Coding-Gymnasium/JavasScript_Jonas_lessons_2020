c4618781005d7a25fff10f63481d26666a8c2e69
Nico Rithner <nicorithner@gmail.com>
2021-02-04 18:16:01 -0700

    Complete lesson 128

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+/*
 const bookings = [];
 
 const createBooking = function (flightNum, numPassengers = 1, price = 199 * numPassengers) {
@@ -22,4 +22,39 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
-				
+*/
+
+const flight = 'LH234';
+const jonas = {
+				name: 'Jonas Schmedtmann',
+				passport: 24739479284,
+}
+
+const checkIn = function(flightNum, passenger) {
+				flightNum = 'LH999';
+				passenger.name = 'Mr. ' + passenger.name;
+
+				if(passenger.passport === 24739479284) {
+								alert('Checked in')
+				} else {
+								alert('Wrong passport!')
+				}
+}
+
+//checkIn(flight, jonas);
+//console.log(flight);
+//console.log(jonas);
+//
+//// is the same as doing
+//const flightNum = flight;
+//const passenger = jonas;
+
+const newPassport = function(person) {
+				person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+
+
+
