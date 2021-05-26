2276e7678aa9d3ea851e45f73b70ef402db5fed2
Nico Rithner <nicorithner@gmail.com>
2021-05-26 11:43:23 -0600

    Complete Guess My Number project

--- 05-Guess-My-Number/starter/script1.js
+++ 05-Guess-My-Number/starter/script1.js
@@ -8,43 +8,51 @@ document.querySelector('.again').addEventListener('click', () => {
 //  window.location.reload();
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start Guessing';
-  document.querySelector('.score').textContent = score
+  displayMessage('Start Guessing...');
+  setScore(score);
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.guess').value = '';
 });
 
-const miss = () => {
-  if(score > 1){
-    score--; 
-    document.querySelector('.score').textContent = score;
-  } else {
-    document.querySelector('.message').textContent = '💀👎 You Lost!';
-  document.querySelector('body').style.backgroundColor = '#8B0000';
-    document.querySelector('.score').textContent = 0;
-  }
+const displayMessage = (message) => {
+  document.querySelector('.message').textContent = message;
+};
+
+const setScore = (currentScore) => {
+  document.querySelector('.score').textContent = currentScore;
 }
+//=======================
+//=======================
 
 document.querySelector('.check').addEventListener('click', function() {
   const guess = Number(document.querySelector('.guess').value);
+  const miss = () => {
+    if(score > 1){
+      displayMessage(guess > secretNumber ? 'Too high!' : 'Too Low!');
+      score--; 
+      setScore(score);
+    } else {
+      displayMessage('💀👎💣 You Lost!');
+    document.querySelector('body').style.backgroundColor = '#8B0000';
+      setScore(0);
+    }
+  }
+
   if(!guess) {
-    document.querySelector('.message').textContent = 'No number!';
+    displayMessage('⛔️ No number!');
   } else if (guess === secretNumber) {
 document.querySelector('.number').textContent = secretNumber;
-    document.querySelector('.message').textContent = 'Correct!';
+    displayMessage('Correct!');
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
-  } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too high!';
-    miss();
-  } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low!';
+  } else if (guess !== secretNumber) {
     miss();
   }
 });
+
