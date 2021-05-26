4c73381790e418a3bd7af1dfa30f9c3ad39d2eac
Nico Rithner <nicorithner@gmail.com>
2021-05-26 13:03:01 -0600

    Add classList actions

--- 06-Modal/starter/script1.js
+++ 06-Modal/starter/script1.js
@@ -9,6 +9,17 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 for(let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', () => {
-    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+    // we don't use the dot when calling classes this way.
   });
 };
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
