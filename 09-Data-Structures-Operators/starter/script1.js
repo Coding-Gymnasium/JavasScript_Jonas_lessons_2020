dfdfa83c3086203503cd5359c7bb5089b0beb29c
Nico Rithner <nicorithner@gmail.com>
2021-05-28 15:49:16 -0600

    Complete lesson 104

--- 09-Data-Structures-Operators/starter/script1.js
+++ 09-Data-Structures-Operators/starter/script1.js
@@ -6,11 +6,68 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
 
   order: function(starterIndex, mainIndex){
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  //orderDelivery: function (obj) {
+  //  console.log(obj);
+  //},
+  orderDelivery: function ({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
+    console.log(`Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`);
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  startIndex: 1,
+  address: 'Via del sole, 21',
+});
+
+const { name, openingHours, categories } = restaurant;
+
+const { name: restaurantName, openingHours: hours, categories: tags } = restaurant;
+
+
+// console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+
+// console.log(menu, starters);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b:7, c: 14 };
+({a, b} = obj); // must wrap between parenthesis
+// console.log(a, b); // changes to 23, 7
+
+//nested object
+const { fri: { open, close } } = openingHours
+// console.log(open, close)
+
 
 /*
 const arr = [2,3,4];
