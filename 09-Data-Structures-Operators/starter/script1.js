87bc4f13406df39c9ab0c8b385ec257a4cc56afa
Nico Rithner <nicorithner@gmail.com>
2021-06-04 06:48:13 -0600

    Complete lesson 117

--- 09-Data-Structures-Operators/starter/script1.js
+++ 09-Data-Structures-Operators/starter/script1.js
@@ -48,7 +48,37 @@ const restaurant = {
 ///////////////////////////////////////////
 ///////////////////////////////////////////
 
+const question = new Map([
+  ['question', 'What is the best language'], [1, 'C'], [2, 'Java'], [3, 'Javascript'], ['correct', 3], [true, 'Correct'], [false, 'Try again']
+]);
+console.log(question);
 
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if(typeof key === 'number'){
+    console.log(`Answer ${key}: ${value}`);
+  };
+}
+
+//const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(question.get(question.get('correct') === answer));
+
+//// Convert map to array
+
+console.log([...question]);
+// console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
+
+/*
 const rest = new Map();
 rest.set('name', 'Classico Italiano');
 rest.set(1, 'Firenze, Italy')
@@ -84,6 +114,7 @@ console.log(rest.get(arr));
 
 rest.set(document.querySelector('h1'), 'Heading');
 console.log(rest);
+*/
 
 /*
 // Sets
