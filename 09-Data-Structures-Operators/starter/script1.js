0db0d28d3df85aa6b4f1d68b4dc6c872d001f1b8
Nico Rithner <nicorithner@gmail.com>
2021-06-06 11:16:09 -0600

    Complete part 1 and 2 of strings lesson

--- 09-Data-Structures-Operators/starter/script1.js
+++ 09-Data-Structures-Operators/starter/script1.js
@@ -48,6 +48,109 @@ const restaurant = {
 ///////////////////////////////////////////
 ///////////////////////////////////////////
 
+// Strings Part 2
+
+const airline = 'TAP Air Portugal';
+
+console.log(airline.toLowerCase());
+console.log(airline.toUpperCase());
+
+// fix capitalization in name
+const passenger = 'jOnAS'; 
+const passengerLower = passenger.toLowerCase();
+const passengerCorrect = passengerLower[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerCorrect);
+
+// comparing emails
+const email = 'hello@jonas.io';
+const loginEmail = ' Hello@Jonas.Io \n';
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimmedEmail = lowerEmail.trim();
+// console.log(trimmedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+
+// replacing
+const priceGB = '288,97£';
+const priceUS = priceGB.replace('£', '$').replace(',', '.');
+console.log(priceUS);
+
+const announcement = 'All passengers come to boarding door 23. Boarding door 23!';
+
+console.log(announcement.replace(/door/g, 'gate')); // With regex
+console.log(announcement.replaceAll('door', 'gate'));
+
+// Booleans
+const plane = 'Airbus A320neo';
+console.log(plane.includes('A320')); // true
+console.log(plane.includes('Boeing'));
+console.log(plane.startsWith('Air'));
+console.log(plane.startsWith('Airbus'));
+
+if(plane.startsWith('Airbus') && plane.endsWith('neo')){
+  console.log('Part of the new airbus family');
+};
+
+// Practice exercise
+//
+const checkBaggage = function(items){
+  const baggage = items.toLowerCase();
+  if(baggage.includes('knife') || baggage.includes('gun')) {
+    console.log('You are NOT allowed on board');
+  } else {
+    console.log('Welcome aboard!');
+  }
+};
+
+checkBaggage('I have a laptop, some Food and a pocket Knife');
+checkBaggage('Socks and camera');
+checkBaggage('Got some snacks and a gun for protection');
+
+
+/*
+// Strings
+
+const airline = 'TAP Air Portugal';
+const plane = 'A320';
+
+console.log(plane[0]); // A
+console.log(airline.length); // 16
+
+console.log(airline.indexOf('r')); // 6
+console.log(airline.lastIndexOf('r')); // 10
+
+console.log(airline.indexOf('Portugal')); // 8
+console.log(airline.slice(4)); // specifiies the begin parameter (index) => Air Portugal
+console.log(airline.slice(4, 7)); // index 7 is the stop parameter => Air
+
+console.log(airline.slice(0, airline.indexOf(' ')));
+console.log(airline.slice(airline.lastIndexOf(' ') + 1));
+
+console.log(airline.slice(-2)); // al
+console.log(airline.slice(1, -2)); // AP Air Portuga
+
+const checkMiddleSeat = function(seat) {
+  // B and E are middle seats.
+  const s = seat.slice(-1);
+  if(s === 'B' || s === 'E') {
+    console.log('You got the middle seat')
+  } else {
+    console.log('You got lucky');
+  };
+};
+
+checkMiddleSeat('11B')
+checkMiddleSeat('23C')
+checkMiddleSeat('3E')
+
+console.log(new String('jonas'));
+console.log(typeof new String('jonas'));
+console.log(typeof new String('jonas').slice());
+*/
+
+/*
 const question = new Map([
   ['question', 'What is the best language'], [1, 'C'], [2, 'Java'], [3, 'Javascript'], ['correct', 3], [true, 'Correct'], [false, 'Try again']
 ]);
@@ -76,7 +179,7 @@ console.log([...question]);
 // console.log(question.entries());
 console.log([...question.keys()]);
 console.log([...question.values()]);
-
+*/
 
 /*
 const rest = new Map();
