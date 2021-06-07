1d5b64fd72487c78f7498241f9c45269b28da324
Nico Rithner <nicorithner@gmail.com>
2021-06-07 10:54:25 -0600

    Complete lesson 130

--- 10-Functions/starter/script1.js
+++ 10-Functions/starter/script1.js
@@ -1,5 +1,35 @@
 'use strict';
 
+const oneWord = function(str) {
+  return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function(str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+}
+
+// Higher order function (it takes a function)
+const transformer = function(str, fn) {
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callbacks all the time
+const high5 = function() {
+  console.log('👋🏻');
+}
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+
+
+
+/*
 const flight = 'LH234';
 const jonas = {
   name: 'Jonas Schmedtmann',
@@ -31,11 +61,7 @@ const newPassport = function(person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
-
-
-
-
-
+*/
 
 /*
 const bookings = [];
