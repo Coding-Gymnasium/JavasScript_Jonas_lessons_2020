bac2b2e4d65e1b454add7752a4c21cfa37e3bfd5
Nico Rithner <nicorithner@gmail.com>
2021-06-21 21:41:09 -0600

    Complete lesson 172

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -397,6 +397,7 @@ console.log(10 / 3); // 3.3333333
 //// Dates
 
 const now = new Date();
+
 console.log(now);
 
 console.log(new Date('Aug 02 2020 18:05:41'));
@@ -406,4 +407,29 @@ console.log(new Date(2037, 10, 19, 15, 23, 5));
 // month in java script is 0 base. 10 is November.
 console.log(new Date(2037, 10, 31));
 // Because there is no November 31st javascript auto corrects to December 1st.
+
+console.log('-----------');
+console.log(new Date(0));
+console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+// working with dates
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(future);
+
+
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+console.log(future.toISOString());
+console.log(future.getTime());
+
+console.log(new Date(2142282180000));
+console.log(Date.now()); // returns a time stamp
+
+future.setFullYear(2040);
+console.log(future);
 
