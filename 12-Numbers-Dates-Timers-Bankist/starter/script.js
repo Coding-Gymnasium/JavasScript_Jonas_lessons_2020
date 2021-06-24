b73b8cef1755a27d811f3559cb517ae2d6490810
Nico Rithner <nicorithner@gmail.com>
2021-06-23 21:38:06 -0600

    Start lesson 174

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -477,4 +477,15 @@ future.setFullYear(2040);
 console.log(future);
 */
 
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+// console.log(Number(future));
+
+const calcDaysPassed = (date1, date2) => Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);;
+
+const days1 = calcDaysPassed(new Date(2037, 3, 14), new Date(2037, 3, 24));
+console.log(days1);
+
+// for more specific dates and to include daylight savings use library moment.js instead
+
 
