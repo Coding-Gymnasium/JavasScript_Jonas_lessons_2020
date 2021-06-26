4e0caa1e3e815238279a6c1b18550dd967767baa
Nico Rithner <nicorithner@gmail.com>
2021-06-26 05:53:14 -0600

    Complete lesson 177

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -290,15 +290,17 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    // Add movement
-    currentAccount.movements.push(amount);
+    setTimeout( function () {
+      // Add movement
+      currentAccount.movements.push(amount);
 
-    // Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
+      // Add loan date
+      currentAccount.movementsDates.push(new Date().toISOString());
 
-    // Update UI
-    updateUI(currentAccount);
-  }
+      // Update UI
+      updateUI(currentAccount);
+    }, 2500);
+}
   inputLoanAmount.value = '';
 });
 
@@ -530,6 +532,7 @@ console.log(future);
 
 // for more specific dates and to include daylight savings use library moment.js instead
 
+/*
 const num = 3884764.23;
 
 const options = {
@@ -545,3 +548,21 @@ console.log('US', new Intl.NumberFormat('en-US', options).format(num));
 console.log('Germany', new Intl.NumberFormat('de-DE', options).format(num));
 console.log('Syria', new Intl.NumberFormat('ar-SY', options).format(num));
 console.log('Browser', new Intl.NumberFormat(navigator.language, options).format(num));
+*/
+
+// setTimeout()
+
+// const ingredients = ['olives', 'spinach']
+const ingredients = ['olives', 'cheese']
+
+const pizzaTimer = setTimeout((ing1, ing2) => console.log(`Here is your pizza 🍕 with ${ing1} and ${ing2}`), 3000, ...ingredients);
+console.log('Waiting...');
+
+if(ingredients.includes('spinach')) clearTimeout(pizzaTimer);
+
+// setInterval()
+setInterval(function() {
+  const now = new Date();
+  console.log(now.toLocaleTimeString());
+}, 1000);
+
