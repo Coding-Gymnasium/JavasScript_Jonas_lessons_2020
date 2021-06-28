5b8757392d0220a6db761968a2e3f363c7cd3f48
Nico Rithner <nicorithner@gmail.com>
2021-06-28 07:26:13 -0600

    Complete lesson 183

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,47 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//------------  Lecture --------------------//
+
+//// Selecting Elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+
+console.log(allSections);
+
+document.getElementById('section--1');
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+document.getElementsByClassName('btn');
+
+// Creating and inserting elements
+// .insertAdjacentHTML
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML = 'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it!</button>'; 
+
+// header.prepend(message); // add the element as the first child
+header.append(message); // adds the element as the last child.
+
+// header.append(message.cloneNode(true));
+//
+// header.before(message);
+// header.after(message);
+
+//// Delete Elements
+
+document.querySelector('.btn--close-cookie').addEventListener('click', function() {
+  // message.parentElement.removeChild(message);
+  message.remove();
+});
