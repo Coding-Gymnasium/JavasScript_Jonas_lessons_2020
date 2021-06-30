ac3f8002d061b41bda450d2b28a90e32bac1c236
Nico Rithner <nicorithner@gmail.com>
2021-06-29 22:18:45 -0600

    Complete lesson 184

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -73,3 +73,52 @@ document.querySelector('.btn--close-cookie').addEventListener('click', function(
   // message.parentElement.removeChild(message);
   message.remove();
 });
+
+// Styles
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.height); // we get nothing because the style is not inline.
+console.log(message.style.backgroundColor); // rgb(55, 56, 61) // We get a result because it is an inline style.
+
+console.log(getComputedStyle(message).color);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+
+logo.alt = 'Beautiful minimalist logo';
+
+// Non-standard
+console.log(logo.getAttribute('designer'));
+
+logo.setAttribute('company', 'Bankist');
+
+console.log(logo.getAttribute('src'));
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.href);
+console.log(link.getAttribute('href'));
+
+// Data Attributes
+
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c', 'j');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+// Don't use
+logo.className = 'jonas';
+
+
