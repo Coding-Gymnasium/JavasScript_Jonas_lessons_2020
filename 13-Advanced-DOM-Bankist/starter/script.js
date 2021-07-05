964eb0bce6fd7ffb5242134190ff60884ce2469e
Nico Rithner <nicorithner@gmail.com>
2021-07-05 13:08:28 -0600

    Complete lesson 188. Capturing and Bubbling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -150,6 +150,7 @@ btnScrollTo.addEventListener('click', function(e) {
     section1.scrollIntoView({ behavior: 'smooth' });
 });
 
+/*
 const h1 = document.querySelector('h1');
 const alertH1 = function(e) {
    alert('addEventListener: Great! You are reading the heading :-)');
@@ -167,4 +168,31 @@ setTimeout(() =>  h1.removeEventListener('mouseenter', alertH1), 3000);
 // };
 // 
 
+*/
+
+// rgb(255,255,255)
+const randomInt = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const randomColor = () => `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})` ;
+
+// console.log(randomColor(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('LINK', e.target, e.currentTarget);
+  console.log(e.currentTarget === this);
+
+  // Stop propagation
+  //e.stopPropagation();
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Container', e.target, e.currentTarget);
+});
+
+document.querySelector('.nav').addEventListener('click', function (e) {
+  this.style.backgroundColor = randomColor();
+  console.log('Nav', e.target, e.currentTarget);
+});
 
