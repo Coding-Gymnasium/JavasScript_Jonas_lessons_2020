9420ab7c89f4765a0803587c8e719345f8325e56
Nico Rithner <nicorithner@gmail.com>
2021-07-24 09:43:35 -0600

    Complete lesson 2005

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const Person = function(firstName, birthYear) {
+  // console.log(this); // => Person {}
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+//  // Bad practice. Don't create a method inside a constructor
+//  this.calcAge = function() {
+//    console.log(2037 - this.birthYear);
+//  }
+}
+
+const jonas = new Person('Jonas', 1991);
+console.log(jonas); // => Person {firstName: "Jonas", birthYear: 1991}
+
+// 1. New empty object {} is created
+// 2. Function is called, this = {}
+// 3. Object linked to prototype
+// 4. function automatically returns object
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(jonas instanceof Person); // => true
