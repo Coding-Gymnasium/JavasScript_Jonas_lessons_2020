4fe2f88aa36f794b3a2cdeb684f491834a771916
Nico Rithner <nicorithner@gmail.com>
2021-07-26 09:13:20 -0600

    Complete lesson 206

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -24,3 +24,27 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person); // => true
+
+// Prototypes
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+jonas.calcAge();
+matilda.calcAge();
+jack.calcAge();
+
+console.log(jonas.__proto__);
+console.log(jonas.__proto__=== Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(jonas)); // true
+console.log(Person.prototype.isPrototypeOf(Person)); // false
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(jonas, matilda); // the species property is in the __proto__
+console.log(jonas.species, matilda.species);
+console.log(jonas.hasOwnProperty('firstName')); // true
+console.log(jonas.hasOwnProperty('species')); // false
+
