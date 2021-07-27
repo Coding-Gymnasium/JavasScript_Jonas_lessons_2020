00530dd2175e76653a8ff2d6c2f76daba8e9002f
Nico Rithner <nicorithner@gmail.com>
2021-07-27 09:14:02 -0600

    Complete lesson 209

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const Person = function(firstName, birthYear) {
   // console.log(this); // => Person {}
   this.firstName = firstName;
@@ -71,3 +71,40 @@ console.log(arr.unique());
 const h1 = document.querySelector('h1');
 console.dir(h1) // to observe the object.
 console.dir(x => x + 1);
+*/
+
+///////////////////////////////////////
+//----------------- Coding Challenge #1
+
+/*
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+
+const Car = function( make, speed) {
+  this.make = make;
+  this.speed = speed;
+  }
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedes', 95);
+console.log(car1);
+console.log(car2);
+
+Car.prototype.accelerate = function() {
+  console.log(`${this.speed += 10} km/h`);
+}
+
+Car.prototype.break = function() {
+  console.log(` ${this.speed -= 5} km/h`);
+}
+
+car1.accelerate();
+car1.break();
