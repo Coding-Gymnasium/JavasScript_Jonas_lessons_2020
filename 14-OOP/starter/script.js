97776edc8fbdf50971acea53b2bf5294f7562570
Nico Rithner <nicorithner@gmail.com>
2021-08-07 22:04:22 -0600

    Complete lesson 219

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -395,6 +395,39 @@ class StudentCl extends PersonCl {
 const martha = new StudentCl('Martha Jones', 2012, 'Computer Science')
 martha.introduce();
 martha.calcAge();
+
+
+//--------------------------------------------
+  // Inheritance Between "Classes": Object.create
+//--------------------------------------------
+
+  const PersonProto = {
+    calcAge() {
+      console.log(2037 - this.birthYear);
+    },
+
+    init(firstName, birthYear) {
+      this.firstName = firstName;
+      this.birthYear = birthYear;
+    },
+  };
+
+const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function(firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function() {
+  console.log(`My name is ${this.fullName} and ( study ${this.course}`);
+}
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+jay.introduce();
+jay.calcAge();
 */
 
 class Account {
@@ -437,5 +470,4 @@ acc1.requestLoan(1000);
 
 
 console.log(acc1);
-
 
