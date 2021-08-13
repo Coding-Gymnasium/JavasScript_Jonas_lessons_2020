bfec120405a5ab2044efc044381a4a40543505c5
Nico Rithner <nico@altvia.com>
2021-08-13 16:07:51 -0600

    Complete lesson 221
    Co-authored-by: Nico nicorithner@gmail.com

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -432,36 +432,46 @@ jay.introduce();
 jay.calcAge();
 */
 
+  // 1) Public fields
+  // 2) Private fields
+  // 3) Public methods
+  // 4) Private methods
+  // (there is also the static version)
+
 class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
     // protected property
-    this._pin = pin;
-    this._movements = [];
-    this.locale = navigator.language;
+    this.#pin = pin;
+    //this._movements = [];
+    //this.locale = navigator.language;
 
     console.log(`thanks for opening an account, ${owner}`);
   }
-  
-  // Public interface
 
+  // 3) Public Methods
+
+  // Public interface
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
 
   deposit(val) {
-    this._movements.push(val)
+    this.#movements.push(val)
   }
 
   withdraw(val) {
     this.deposit(-val)
   }
 
-  _approveLoan(val) {
-    return true;
-  }
 
   requestLoan(val) {
     if(this._approveLoan(val)) {
@@ -469,14 +479,28 @@ class Account {
       console.log('Loan approvad');
     }
   }
+
+  static helper () {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+
 }
 
 const acc1 = new Account('Jonas', 'EUR', 1111);
 
 acc1.deposit(250);
-acc1._movements.push(240);
+//acc1._movements.push(240);
 acc1.withdraw(140);
 acc1.requestLoan(1000);
 console.log(acc1.getMovements());
-
 console.log(acc1);
+
+//console.log(acc1.#movements);
+//console.log(acc1.#pin);
+Account.helper();
