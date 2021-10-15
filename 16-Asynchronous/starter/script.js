4779e8bdfc36ea4e34859139482ceb35072b2060
Nico Rithner <nicorithner@gmail.com>
2021-10-15 12:52:18 -0600

    Complete lesson 262

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -521,6 +521,7 @@ createImage('img/img-3.jpg')
 
 */
 
+/*
 //----------- Async Await
 const getPosition = () => {
   return new Promise((resolve, reject) => {
@@ -574,3 +575,23 @@ console.log('FIRST');
   }
   console.log('finished getting location');
 })();
+*/
+
+const get3Countries = async (c1, c2, c3) => {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
