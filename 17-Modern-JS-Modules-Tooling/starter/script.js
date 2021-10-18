0cf4a559f63f5dfab4b9287c0d16c487b874e29f
Nico Rithner <nicorithner@gmail.com>
2021-10-18 10:37:28 -0600

    Complete lesson 270

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,3 +1,4 @@
+/*
 // import * as ShoppingCart from './shoppingCart.js';
 import quantitySum, * as ShoppingCart from './shoppingCart.js';
 // import { addToCart, totalPrice as Price, tq } from './shoppingCart.js';
@@ -15,8 +16,40 @@ ShoppingCart.addToCart('banana', 35);
 quantitySum(); // quantitySum is an alias for addtotal because the way I imported it.
 
 console.log(ShoppingCart.cart);
+*/
 
 /**
- * Typically is not good practice to 
+ * Typically is not good practice to
  * mix name exports and default exports
  */
+
+// ----- Module Pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = (product, quantity) => {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart and shipping cost is ${shippingCost}`
+    );
+  };
+
+  const orderStock = (product, quantity) => {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pear', 3);
